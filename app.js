7d629257bad78519e91892ca32d26d79f7efc1a1
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2020-12-24 16:13:45 -0500

    login page done

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+import path from "path";
 import express from "express";
 import { requireLogin } from "./middleware.js";
+import loginRouter from "./routes/loginRoutes.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", "views");
+
+console.log(__dirname)
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", requireLogin, (req, res, next) => {
   const payload = {
@@ -13,6 +24,9 @@ app.get("/", requireLogin, (req, res, next) => {
   res.render("home", payload);
 });
 
-const PORT = process.env.PORT || 3003;
+// MOUNT ROUTES
+app.use("/login", loginRouter);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
