61a7bb22aa4857d14f663ef8f08bf3821fe64f7a
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2020-12-24 13:16:30 -0500

    login middleware

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from "express";
+import { requireLogin } from "./middleware.js";
 
+const app = express();
 
-const app = express()
+app.set("view engine", "pug");
+app.set("views", "views");
 
+app.get("/", requireLogin, (req, res, next) => {
+  const payload = {
+    pageTitle: "home page",
+  };
+  res.render("home", payload);
+});
 
+const PORT = process.env.PORT || 3003;
 
-const PORT = process.env.PORT || 3003
-
-app.listen(PORT, ()=>console.log(`listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
