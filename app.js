5413c9466eda7061262ff273ad268f1d0369277c
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2020-12-25 03:34:08 -0500

    creating the users model

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ import loginRouter from "./routes/loginRoutes.js";
 import registerRouter from "./routes/registerRoutes.js";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+
+//config
+dotenv.config();
 
 //body parser
 app.use(express.json());
@@ -18,7 +22,13 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.set("views", "views");
 
-console.log(__dirname);
+// connect to db
+mongoose.connect(process.env.DATABASE_LOCAL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+}).then(()=>console.log('DB connected'));
 
 app.use(express.static(path.join(__dirname, "public")));
 
---
+++ models/userModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import validator from 'validator'
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, 'Enter a valid email']
+    },
+    image: {
+        type: String,
+        default: '/images/profile.png'
+    },
+    coverImage: {
+        type: String
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
