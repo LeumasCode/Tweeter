b3838252559d123fa772b4587a4c9781aa4b57a9
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2020-12-25 02:51:14 -0500

    adding server side validation

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -6,10 +6,18 @@ router.get("/", (req, res, next) => {
   res.status(200).render("register");
 });
 
-
 router.post("/", (req, res, next) => {
-    console.log(req.body)
-  res.status(200).render("register");
+  const { firstName, lastName, email, password } = req.body;
+
+  const payload = req.body
+
+  if(firstName && lastName && email && password){
+    
+  }else{
+      payload.errorMessage = 'Make sure each field has a valid value'
+       res.status(200).render("register", payload);
+  }
+ 
 });
 
 export default router;
