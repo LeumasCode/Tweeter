71b09e881d03409dac5ddc0d696e69978239b75a
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2020-12-26 15:52:57 -0500

    adding the post schema

---
+++ models/postModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const postSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      trim: true,
+    },
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    pinned: Boolean,
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
