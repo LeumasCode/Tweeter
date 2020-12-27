b8ee82af702bea1d986b64b47b16c20333c22b5a
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2020-12-27 13:49:50 -0500

    displaying contents

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,18 @@
 $(document).ready(() => {
-  $.get("/api/posts", (result) => {
-    console.log(result);
+  $.get("/api/posts", (results) => {
+    outputPosts(results, $(".postsContainer"));
   });
 });
+
+function outputPosts(results, container) {
+  container.html("");
+
+  results.forEach((result) => {
+    const html = createPostHtml(result);
+    container.append(html);
+  });
+
+  if(results.length == 0){
+      container.append("<span class='noResult'>Nothing to show</span>")
+  }
+}
