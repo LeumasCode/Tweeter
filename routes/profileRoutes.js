5ebcd62dbf65b553ec27ce40b65a9972dc0faffd
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2020-12-30 10:49:06 -0500

    accessing profile by username

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getPost } from "../controllers/postController.js";
+import User from "../models/userModel.js";
 
 const router = express.Router();
 
@@ -8,9 +9,37 @@ router.get("/", (req, res, next) => {
     pageTitle: req.session.user.username,
     userLoggedIn: req.session.user,
     userLoggedInJs: JSON.stringify(req.session.user),
-    profileUser: req.session.user
+    profileUser: req.session.user,
   };
+
+  
   res.status(200).render("profilePage", payload);
 });
+
+router.get("/:username", async (req, res, next) => {
+  const payload = await getPayload(req.params.username, req.session.user)
+  res.status(200).render("profilePage", payload);
+});
+
+async function getPayload(username, userLoggedIn) {
+  const user = await User.findOne({ username });
+
+  if (user == null) {
+    return {
+      pageTitle: "User not found",
+      userLoggedIn,
+      userLoggedInJs: JSON.stringify(userLoggedIn),
+    };
+  }
+
+
+
+  return {
+    pageTitle: user.username,
+    userLoggedIn,
+    userLoggedInJs: JSON.stringify(userLoggedIn),
+    profileUser: user,
+  };
+}
 
 export default router;
