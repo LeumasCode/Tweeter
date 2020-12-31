b6da9ad4d65c422704e82ce41522450e1c9e644e
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2020-12-31 09:24:13 -0500

    creating followers and following page

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -22,11 +22,24 @@ router.get("/:username", async (req, res, next) => {
 });
 
 
+router.get("/:username/following", async (req, res, next) => {
+  const payload = await getPayload(req.params.username, req.session.user);
+   payload.selectedTab = "following";
+  res.status(200).render("followersAndFollowing", payload);
+});
+
+router.get("/:username/followers", async (req, res, next) => {
+  const payload = await getPayload(req.params.username, req.session.user);
+  payload.selectedTab = "followers";
+  res.status(200).render("followersAndFollowing", payload);
+});
+
 router.get("/:username/replies", async (req, res, next) => {
   const payload = await getPayload(req.params.username, req.session.user);
-   payload.selectedTab = "replies";
+  payload.selectedTab = "replies";
   res.status(200).render("profilePage", payload);
 });
+
 
 async function getPayload(username, userLoggedIn) {
   let user = await User.findOne({ username });
