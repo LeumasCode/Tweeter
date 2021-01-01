eafa94f0cae6ac3f44a449ac2dcd2c2246824bea
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-01 14:03:18 -0500

    outputing the user

--- public/js/followersAndFollowing.js
+++ public/js/followersAndFollowing.js
@@ -1,10 +1,7 @@
 function loadFollowers() {
-  $.get(
-    `/api/profile/users/${profileUserId}/followers`,
-    (results) => {
-      outputUsers(results, $(".postsContainer"));
-    }
-  );
+  $.get(`/api/profile/users/${profileUserId}/followers`, (results) => {
+    outputUsers(results.followers, $(".resultsContainer"));
+  });
 }
 
 function loadFollowing() {
@@ -12,13 +9,36 @@ function loadFollowing() {
     `/api/profile/users/${profileUserId}/following`,
 
     (results) => {
-      outputUsers(results, $(".resultsContainer"));
+      outputUsers(results.following, $(".resultsContainer"));
     }
   );
 }
 
-function outputUsers(data, container) {
-    console.log(data)
+function outputUsers(results, container) {
+  container.html("");
+
+  results.forEach((result) => {
+    let html = createUserHtml(result, true);
+    container.append(html);
+  });
+  if (results.length == 0) {
+    container.append("<span class='noResults'>No results found</span>");
+  }
+}
+
+function createUserHtml(userData, showFollowButton) {
+  let name = userData.firstName + "" + userData.lastName;
+  return `<div class='user'>
+                <div class='userImageContainer'>
+                    <img src='${userData.image}'>
+                </div>
+                <div class='userDetailsContainer'>
+                    <div class='header'>
+                        <a href='/profile/${userData.username}'>${name}</a>
+                        <span class='username'>@${userData.username}</span>
+                    </div>
+                </div>
+            </div>`;
 }
 
 $(document).ready(() => {
