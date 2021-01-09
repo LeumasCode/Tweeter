73556437e6af40b606bfdca79816dcab7b86a1a6
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-09 14:11:16 -0500

    outputting chat elements

--- public/js/inboxPage.js
+++ public/js/inboxPage.js
@@ -5,5 +5,26 @@ outputChatList(data, $('.resultsContainer'))
 })
 
 function outputChatList(chatList, container){
-    console.log(chatList)
+    chatList.forEach((chat)=> {
+        let html = createChatHtml(chat)
+        container.append(html)
+    })
+
+    if(chatList.length == 0) {
+        container.append("<span class='noResults'>Nothing to show</span>")
+    }
+}
+
+function createChatHtml(chatData){
+let chatName = 'Chat Name' // to do later
+let image = ''; // to do later
+let latestMessage = 'this is latest message'
+
+return `<a href='/messages/${chatData._id}' class='resultListItem'>
+            <div class='resultDetailsContainer'>
+                <span class='heading'>${chatName}</span>
+                <span class='heading'>${latestMessage}</span>
+            </div>
+
+        </a>`;
 }
