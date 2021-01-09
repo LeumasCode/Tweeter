d6e16c4088953b59164eace4c6822adae149d0b8
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-09 13:02:36 -0500

    creating the chat schema

---
+++ models/chatModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+
+const chatSchema = new mongoose.Schema(
+  {
+    chatName: {
+      type: String,
+      trim: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "Users" }],
+    latestMessage: {
+      type: { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
+    },
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.model("Chat", chatSchema);
+
+export default Chat;
