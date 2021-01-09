dee29cb655e6ea12dc3d7ab2f3a03a5f12b4be53
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-09 13:34:48 -0500

    creating chats

--- routes/api/chats.js
+++ routes/api/chats.js
@@ -1,37 +1,42 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import User from "../../models/userModel.js";
-import Post from "../../models/postModel.js";
+import Chat from "../../models/chatModel.js";
 
 const router = express.Router();
 
 router.post(
   "/",
   asyncHandler(async (req, res, next) => {
-    const { users } = req.body;
+    let { users } = req.body;
 
-    if (!content) {
-      console.log("content not sent");
-      return res.sendStatus(400);
-    }
-
-    if (!req.session.user) {
-      console.log("User not logged in!");
-      res.redirect("/logout");
+    if (!users) {
+      console.log("users not sent from client");
       return;
     }
 
-    const post = await Post.create({
-      content,
-      postedBy: req.session.user,
-      replyTo,
+    //convert the users back to object
+    users = JSON.parse(users);
+
+    if (users.length == 0) {
+      console.log("users array is empty");
+      return;
+    }
+
+    users.push(req.session.user); // push ourself inside the array
+
+    let chatData = {
+      users,
+      isGroupChat: true,
+    };
+
+    const chat = await Chat.create({
+      users,
+      isGroupChat: true,
     });
-    await post.populate("postedBy").execPopulate();
-    res.status(201).send(post);
+
+    res.status(201).send(chat);
   })
 );
-
-
-
 
 export default router;
