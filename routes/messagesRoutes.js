36879ea728d76664254e03063fd9db8ee2642b3b
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-10 13:57:33 -0500

    getting the chat data

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getPost } from "../controllers/postController.js";
+import Chat from "../models/chatModel.js";
 import User from "../models/userModel.js";
 
 const router = express.Router();
@@ -20,6 +21,28 @@ router.get("/new", async (req, res, next) => {
     userLoggedInJs: JSON.stringify(req.session.user),
   };
   res.status(200).render("newMessage", payload);
+});
+
+router.get("/:chatId", async (req, res, next) => {
+  let userId = req.session.user;
+  let chatId = req.params.chatId;
+
+  let chat = await Chat.findOne({
+    _id: chatId,
+    users: { $elemMatch: { $eq: userId } },
+  }).populate("users");
+
+  if (chat == null) {
+    // check if chat id is really user id
+  }
+
+  const payload = {
+    pageTitle: "Chat",
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+    chat,
+  };
+  res.status(200).render("chatPage", payload);
 });
 
 export default router;
