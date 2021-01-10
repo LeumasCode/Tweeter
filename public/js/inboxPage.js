f3888c1821dbe98601bfc6a5270eb551a2e2d2aa
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-10 01:55:35 -0500

    showing chat image

--- public/js/inboxPage.js
+++ public/js/inboxPage.js
@@ -17,10 +17,11 @@ function outputChatList(chatList, container) {
 
 function createChatHtml(chatData) {
   let chatName = getChatName(chatData);
-  let image = ""; // to do later
+  let image = getChatImageElements(chatData);
   let latestMessage = "this is latest message";
 
   return `<a href='/messages/${chatData._id}' class='resultListItem'>
+            ${image}
             <div class='resultsDetailsContainer'>
                 <span class='heading'>${chatName}</span>
                 <span class='subText'>${latestMessage}</span>
@@ -33,8 +34,10 @@ function getChatName(chatData) {
   let chatName = chatData.chatName;
   if (!chatName) {
     let otherChatUsers = getOtherChatUsers(chatData.users);
-    let namesArray = otherChatUsers.map(user =>user.firstName + " " + user.lastName);
-    chatName = namesArray.join(",")
+    let namesArray = otherChatUsers.map(
+      (user) => user.firstName + " " + user.lastName
+    );
+    chatName = namesArray.join(",");
   }
 
   return chatName;
@@ -44,4 +47,26 @@ function getOtherChatUsers(users) {
   if (users.length == 1) return users;
 
   return users.filter((user) => user._id != userLoggedIn._id);
+}
+
+function getChatImageElements(chatData) {
+  let otherChatUsers = getOtherChatUsers(chatData.users);
+
+  let groupChatClass = "";
+
+  let chatImage = getUserChatImageElement(otherChatUsers[0]);
+
+  if (otherChatUsers.length > 1) {
+    groupChatClass = "groupChatImage";
+    chatImage += getUserChatImageElement(otherChatUsers[1]);
+  }
+  return `<div class='resultsImageContainer ${groupChatClass}'>${chatImage}</div>`;
+}
+
+function getUserChatImageElement(user) {
+  console.log(user);
+  if (!user || !user.image) {
+    return alert("User passed into function is invalid");
+  }
+  return `<image src='${user.image}' alt='Users profile pic'></image>`;
 }
