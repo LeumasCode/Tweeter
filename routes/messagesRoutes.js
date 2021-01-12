79883d54f899afabdd5fefa075b4e86572353734
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-12 11:24:46 -0500

    check if chat is valid

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import { getPost } from "../controllers/postController.js";
 import Chat from "../models/chatModel.js";
 import User from "../models/userModel.js";
@@ -27,6 +28,19 @@ router.get("/:chatId", async (req, res, next) => {
   let userId = req.session.user;
   let chatId = req.params.chatId;
 
+  let isValidId = mongoose.isValidObjectId(chatId);
+
+  const payload = {
+    pageTitle: "Chat",
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+  };
+
+  if (!isValidId) {
+    payload.errorMessage = "you do not have permission to view it";
+    return res.status(200).render("chatPage", payload);
+  }
+
   let chat = await Chat.findOne({
     _id: chatId,
     users: { $elemMatch: { $eq: userId } },
@@ -34,14 +48,19 @@ router.get("/:chatId", async (req, res, next) => {
 
   if (chat == null) {
     // check if chat id is really user id
+    let userFound = await User.findById(chatId);
+
+    if (userFound != null) {
+      // get chat using user id
+    }
   }
 
-  const payload = {
-    pageTitle: "Chat",
-    userLoggedIn: req.session.user,
-    userLoggedInJs: JSON.stringify(req.session.user),
-    chat,
-  };
+  if (chat == null) {
+    payload.errorMessage = "you do not have permission to view it";
+  } else {
+    payload.chat = chat;
+  }
+
   res.status(200).render("chatPage", payload);
 });
 
