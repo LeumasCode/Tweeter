c418a5f20b48b7ed6f01bbfb48a72ef8b3ba100b
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-13 12:15:05 -0500

    making ajax call

---
+++ public/js/chatPage.js
@@ -0,0 +1,16 @@
+$("#chatNameButton").click(() => {
+  let name = $("#chatNameTextbox").val().trim();
+
+  $.ajax({
+    url: `http://localhost:5000/api/chat/${chatId}`,
+    type: "PUT",
+    data: { chatName: name },
+    success: (data, status, xhr) => {
+      if (xhr.status == 404) {
+        return alert("could not update");
+      } else {
+        location.reload();
+      }
+    },
+  });
+});
