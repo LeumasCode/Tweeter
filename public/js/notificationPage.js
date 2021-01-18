f0d2477dde2cba6dc52660833ac836ffdf2dd91e
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-18 14:44:50 -0500

    creating notification html

--- public/js/notificationPage.js
+++ public/js/notificationPage.js
@@ -1,5 +1,31 @@
-$(document).ready(()=>{
-    $.get('/api/notifications', (data)=>{
-console.log(data)
-    })
-})
+$(document).ready(() => {
+  $.get("/api/notifications", (data) => {
+    outputNotificationList(data, $(".resultsContainer"));
+  });
+});
+
+function outputNotificationList(notifications, container) {
+  notifications.forEach((notification) => {
+    let html = createNotificationHtml(notification);
+
+    container.append(html);
+  });
+
+  if (notifications.length == 0) {
+    container.append('<span class="noResults">Nothing to show</span>');
+  }
+}
+
+function createNotificationHtml(notification) {
+  let userFrom = notification.userFrom;
+
+  return `<a href='#' class='resultListItem notification'>
+            <div class='resultsImageContainer'>
+                <img src='${userFrom.image}'>
+                
+            </div>
+            <div class='resultsDetailsContainer ellipsis'>
+                <span class= 'ellipsis'>This is the text</span>
+            </div>
+        </a>`;
+}
